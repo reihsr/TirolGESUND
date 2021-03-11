852c42ed8305f9b8e83b80f4c0e88dff7c2b2922
Robert Reihs <robert.reihs@medunigraz.at>
2021-03-11 23:34:35 +0100

    Add a base template for the application

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from jproperties import Properties
 
-app = Flask(__name__)
+configs = Properties()
+with open('config.properties', 'rb') as config_file:
+    configs.load(config_file)
+
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=configs.get("HOST").data, debug=configs.get("DEBUG").data)
